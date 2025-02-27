53df4a35beb1776019000e5786b497752b7d1a86
nelsonkna9 <nelsonkna9@yandex.ru>
2025-02-27 17:47:00 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# fancy-baking-site
+ 
---
+++ script.js
@@ -0,0 +1,7 @@
+// script.js
+
+document.addEventListener("DOMContentLoaded", () => {
+  console.log("Site vitrine chargé et prêt !");
+  // Vous pouvez ajouter ici des fonctionnalités interactives
+  // Par exemple, un listener sur le bouton "ORDER" ou des animations
+});
